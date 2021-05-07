d4ca15000926677dab90b07c9047006c203efd2f
Your Name <naresh.29devops@gmail.com>
2021-05-07 17:46:56 +0530

    new codes built

---
+++ src/test/java/may projects/SampleTestNgTest.java
@@ -0,0 +1,24 @@
+package may projects;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import may projects.pages.HomePage;
+
+public class SampleTestNgTest extends TestNgTestBase {
+
+  private HomePage homepage;
+
+  @BeforeClass
+  public void testInit() {
+    driver.get(baseUrl);
+    homepage = PageFactory.initElements(driver, HomePage.class);
+  }
+
+  @Test
+  public void testHomePageHasAHeader() {
+    Assert.assertFalse("".equals(homepage.header.getText()));
+  }
+}
